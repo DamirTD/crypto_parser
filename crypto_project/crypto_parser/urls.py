500e21443fce6491d46702f5a865630ae62b4239
toarda <tor.archil.dav@gmail.com>
2023-10-13 19:48:43 +0600

    Изменил дизайн и исправил пару ошибок

--- crypto_project/crypto_parser/urls.py
+++ crypto_project/crypto_parser/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cryptocurrency/', views.get_cryptocurrency_info, name='get_cryptocurrency_info'),
+    path('', views.get_cryptocurrency_info, name='get_cryptocurrency_info'),
 ]
--- crypto_project/crypto_parser/views.py
+++ crypto_project/crypto_parser/views.py
@@ -3,27 +3,44 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 
+def find_and_extract(soup, element_type, class_name_selector, class_name_selector_alt, default="Not Found"):
+    element = soup.find(element_type, class_=class_name_selector)
+    if not element:
+        element = soup.find(element_type, class_=class_name_selector_alt)
+
+    if element:
+        return element.text.strip()
+    else:
+        return default
+
 # Функция для парсинга биткоина
 def get_cryptocurrency_info(request):
     crypto_info = None
 
     if request.method == 'POST':
         user_input = request.POST.get('crypto_input', '').strip()
-        url = f'https://coinmarketcap.com/currencies/{user_input}/'
+        url = f'https://coinmarketcap.com/currencies/{user_input.replace(" ", "-")}/'
 
         response = requests.get(url)
 
+        if '-' in user_input:
+            price_class = 'sc-16891c57-0 ksCNvw base-text'
+            price_class_alt = 'sc-16891c57-0 dxubiK base-text'
+        else:
+            price_class = 'sc-16891c57-0 dxubiK base-text'
+            price_class_alt = 'sc-16891c57-0 ksCNvw base-text'
+
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
-            topicID_element = soup.find('span', class_='sc-16891c57-0 dMKlNV base-text')
-            topicName_element = soup.find('span', class_='coin-name-pc')
-            topicPrice_element = soup.find('span', class_='sc-16891c57-0 dxubiK base-text')
+            topicID_element = find_and_extract(soup, 'span', class_name_selector='sc-16891c57-0 dMKlNV base-text', class_name_selector_alt='some-other-class')
+            topicName_element = find_and_extract(soup, 'span', class_name_selector='coin-name-pc', class_name_selector_alt='another-class')
+            topicPrice_element = find_and_extract(soup, 'span', class_name_selector=price_class, class_name_selector_alt=price_class_alt)
 
             if topicID_element and topicName_element and topicPrice_element:
                 crypto_info = {
-                    'id': topicID_element.text.strip(),
-                    'name': topicName_element.text.strip(),
-                    'price': topicPrice_element.text.strip(),
+                    'id': topicID_element,
+                    'name': topicName_element,
+                    'price': topicPrice_element,
                 }
             else:
                 crypto_info = {'error': 'Данные не найдены'}
