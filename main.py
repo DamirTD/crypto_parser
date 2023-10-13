a32047a1a8c1b1fc0d632dcff2d8d1ff4d8c83a6
DamirTD <toriya.damir.2004@gmail.com>
2023-10-13 19:23:05 +0600

    обновлены комментарии

--- main.py
+++ main.py
@@ -2,25 +2,25 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Функция для поиска и извлечения информации из HTML-страницы 
 def find_and_extract(soup, element_type, class_name, default="Not Found"):
-            element = soup.find(element_type, class_=class_name)
-            if element:
-                return element.text.strip()
-            else:
-                return default
+    element = soup.find(element_type, class_=class_name)        # Ищем элемент на странице с заданным типом и классом
+    if element:                                                 # Если нашли элемент. element = topicID, topicName, topicPrice
+        return element.text.strip()
+    else:                                                       # В случае если не нашли элемент
+        return default
 
 while True:
     # Ввод пользователя
     user_input = input("Enter a cryptocurrency ID or Name (or 'exit' to quit): ").strip()
     
-    if user_input.lower() == 'exit':
-        break  # Выйти если пользователь напишет 'exit'
+    if user_input.lower() == 'exit':                            # Убираем заглавные буквы чтобы по итогу получалось 'exit'
+        break                                                   # Выйти если пользователь напишет 'exit'
 
-    user_input = user_input.replace(" ", "-")
-
-    if '-' in user_input:
+    user_input = user_input.replace(" ", "-")                   # Меняем пробел на тире, для того чтобы в url значение input-a было через тире.
+    if '-' in user_input:                                       # Из-за того что классы с пробелом и без разные, то выводить нужно соответственно разные классы
         price_class = 'sc-16891c57-0 ksCNvw base-text'
-    else:
+    else:                                                       # В случае если введенное пользователем значение не имеет пробелов и тире.
         price_class = 'sc-16891c57-0 dxubiK base-text'
 
     # URL сопоставляем с вводом пользователя
@@ -31,6 +31,7 @@ while True:
 
     # Проверяем успешность запроса
     if response.status_code == 200:
+
         # Создаем объект BeautifulSoup для парсинга HTML
         soup = BeautifulSoup(response.text, 'html.parser')
         
